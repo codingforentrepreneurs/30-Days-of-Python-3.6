6d0f198a2bf54b842d11827b00a0e81752a978bc
CFE <hello@teamcfe.com>
2016-06-07 17:37:01 -0700

    Day 6

---
+++ Day 6/day_6_start.py
@@ -0,0 +1,12 @@
+names = ["Justin", "john", "Emilee", "Jim", "Ron", "Sandra", "veronica", "Whitney"]
+amounts = [123.32, 94.23, 124.32, 323.4, 23, 322.122323, 32.4, 99.99]
+
+message = """Hi there! 
+
+Thank you for the purchase on May 5th 2016. 
+We hope you are exicted about using it. Just as a
+reminder the purcase total was $199.99.
+Have a great one!
+
+Team CFE
+"""
