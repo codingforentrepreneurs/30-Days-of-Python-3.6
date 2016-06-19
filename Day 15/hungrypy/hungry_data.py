e2869284d6b14747d4da786a85e8b95b270a94fb
CFE <hello@teamcfe.com>
2016-06-18 21:30:01 -0700

    Day 15/

---
+++ Day 15/hungrypy/hungry_data.py
@@ -0,0 +1,31 @@
+import csv
+
+def get_length(file_path):
+    with open("data.csv") as csvfile:
+        reader = csv.reader(csvfile)
+        reader_list = list(reader)
+        print(reader_list)
+        return len(reader_list)
+
+def append_data(file_path, name, email):
+    fieldnames = ['id', 'name', 'email']
+    #the number of rows?
+    next_id = get_length(file_path)
+    with open(file_path, "a") as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writerow({
+                "id": next_id,
+                "name": name,
+                "email": email,
+            })
+
+
+
+append_data("data.csv", "Justin", "hello@teamcfe.com")
+
+
+
+
+
+
+
